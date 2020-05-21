8415ce5e81da86940aeeb50a56e02a17d5397a63
RandomFlag <45017195+RandomFlag@users.noreply.github.com>
2020-05-21 23:09:04 +0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1590091592,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/Дерево-sheet0.png",
+		"images/bush-sheet0.png",
+		"images/Яблоко-sheet0.png",
+		"images/return-sheet0.png",
+		"images/fairy_bow-sheet0.png",
+		"images/player_hold-sheet0.png",
+		"images/bow-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
